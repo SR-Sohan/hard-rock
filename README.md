8065e369be0399afad981141c3e44751c55ddc25
SR-Sohan <sr.sohanur22@gmail.com>
2020-08-18 00:26:30 +0600

    hard rock done

---
+++ README.md
@@ -0,0 +1,3 @@
+# hard-rock-solution
+api link: https://api.lyrics.ovh/suggest/summer
+# hard-rock
---
+++ main.js
@@ -0,0 +1,83 @@
+
+const showList = document.querySelector('.search-result ');
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', function(){
+    const searchBox = document.getElementById('search-box');
+    const searchValue = searchBox.value;
+    if(!searchValue){
+        alert('please write song name')
+    }else{
+        searchSong(searchValue);
+    }
+    
+})
+
+function searchSong(searchValue){
+    fetch(`https://api.lyrics.ovh/suggest/${searchValue}`)
+    .then(res => res.json())
+    .then(data => {
+        
+        showData(data)
+    })
+}
+function showData(data){
+    let limitData = data.data.slice(0,10);
+    showList.innerHTML =`
+        ${limitData.map(song => `
+        
+        <div class="single-result row align-items-center my-3 p-3">
+            <div class="col-md-3">
+            <img src="${song.album.cover_small}" >
+            </div>
+            <div class="col-md-6 text-center">
+                <h3 class="lyrics-name">${song.title}</h3>
+                <p class="author lead">Album by <span>${song.album.title}</span></p>
+            </div>
+            <div class="col-md-3 text-md-right text-center">
+                <button data-artist="${song.artist.name}" data-song="${song.title}" class="btn btn-success">Get Lyrics</button>
+            </div>
+        </div>
+        `).join('')
+    }
+    `
+
+
+
+}
+
+
+showList.addEventListener('click',e=>{
+    const clickElement = e.target;
+    if(clickElement.tagName === 'BUTTON'){
+        const artist = clickElement.getAttribute('data-artist');
+        const songName = clickElement.getAttribute('data-song');
+        console.log(artist,songName);
+        getLyrics(artist,songName);
+    }
+})
+
+async function getLyrics(artist,songName){
+    const res = await fetch(`https://api.lyrics.ovh/v1/${artist}/${songName}`)
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/\n/g, '<br />');
+   const lyricsShow = document.querySelector('.single-lyrics');
+    
+   if(lyrics == lyrics){
+    lyricsShow.innerHTML = `
+     <button class="btn go-back">&lsaquo;</button>
+     <h2 class="text-success mb-4">${artist} -${songName}</h2>
+     <pre class="lyric text-white">
+         ${lyrics}
+     </pre>
+    `
+ }
+  else {
+        lyricsShow.innerHTML = `
+        <button class="btn go-back">&lsaquo;</button>
+        <h2 class="text-success mb-4">${artist} -${songName}</h2>
+        <pre class="lyric text-white">
+            <h2> lyrics Not Found</h2>
+        </pr`
+    }
+}
